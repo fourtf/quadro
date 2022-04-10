43a65d954757afa2ea3ff3dea5d24b90fe8e33aa
fourtf <tf.four@gmail.com>
2022-04-10 23:01:48 +0200

    allow changing card color

--- cards/card.gd
+++ cards/card.gd
@@ -4,7 +4,7 @@ class_name Card
 
 enum CardColor { Red, Blue, Green, Yellow, Colorless }
 
-var card_materials_by_color = {
+var card_texture_by_color = {
 	CardColor.Red: load("res://images/card_red.png"),
 	CardColor.Blue: load("res://images/card_blue.png"),
 	CardColor.Green: load("res://images/card_green.png"),
@@ -18,7 +18,7 @@ export(CardColor) var color = CardColor.Colorless; var color_
 func _process(delta):
 	if color_ != color:
 		color_ = color
-		$MeshInstance.material_override = card_materials_by_color[color]
+		$MeshInstance.get_surface_material(0).albedo_texture = card_texture_by_color[color]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
